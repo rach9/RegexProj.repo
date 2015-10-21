71efd57498ec7a6b24398b5becd93599f02e936a
rach9 <rajeshchaitu@gmail.com>
2015-10-21 16:46:17 +0200

    - Program modified to handle empty inputs

--- regExpProg.c
+++ regExpProg.c
@@ -7,7 +7,9 @@ int main (int argc, char* argv[])
 {
 
   char input[100 + 1], regExpStr[100 + 1];
-  int ind = 0,retVal = 0, regExpFlag = 0;
+  input[0] = '\0';
+  regExpStr[0] = '\0';
+  int ind = 0,retVal = 0, regExpFlag = 0, inputFlag = 0;
   regex_t re;
   while (ind < argc)
   {
@@ -17,6 +19,7 @@ int main (int argc, char* argv[])
       {
 	strcpy(input, argv[ind+1]);
         input[strlen(argv[ind+1])] = '\0';
+        inputFlag = 1;
       }
     }
     if (strcmp(argv[ind], "-r")==0)
@@ -31,28 +34,29 @@ int main (int argc, char* argv[])
 
     ind++;
   }
-  printf("Input %s, len = %d\n", input,strlen(input));
-
-  if (regcomp(&re, regExpFlag?regExpStr:LARAALNUM, REG_EXTENDED) != 0)
-  {
-    printf("Failed to compile regular exp %s\n", regExpFlag?regExpStr:LARAALNUM);
-    return 0;
-  }
+  printf("Input: %s, len: = %d\n", (inputFlag==1)?input:"Empty Input",strlen(input));
   
-  retVal = regexec(&re, input, 0, NULL, 0);
-  if (retVal == 0)
+  if (inputFlag)
   {
-    printf("Input \"%s\" Confirms to \"%s\" regex\n",input, regExpFlag?regExpStr:LARAALNUM);
-  }
-  else
-    {
+     if (regcomp(&re, regExpFlag?regExpStr:LARAALNUM, REG_EXTENDED) != 0)
+     {
+        printf("Failed to compile regular exp %s\n", regExpFlag?regExpStr:LARAALNUM);
+        return 0;
+     }
+  
+     retVal = regexec(&re, input, 0, NULL, 0);
+     if (retVal == 0)
+     {
+        printf("Input \"%s\" Confirms to \"%s\" regex\n",input, regExpFlag?regExpStr:LARAALNUM);
+     }
+     else
+     {
         char errorBuf[100];
-      printf("!!! Input \"%s\" doesnt confirm to \"%s\" regex !!!\n", input, regExpFlag?regExpStr:LARAALNUM);
-      regerror(retVal, &re, errorBuf, 100);
-      printf("Error Information:  %s\n", errorBuf);
-    }
-
-  regfree(&re);
-
+        printf("!!! Input \"%s\" doesnt confirm to \"%s\" regex !!!\n", input, regExpFlag?regExpStr:LARAALNUM);
+        regerror(retVal, &re, errorBuf, 100);
+        printf("Error Information:  %s\n", errorBuf);
+     }
+     regfree(&re);
+  }
   return 1;
 }
