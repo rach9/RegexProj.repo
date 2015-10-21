5c1e894722dbbe3f78d2147840def40e97bbb9ce
rach9 <rajeshchaitu@gmail.com>
2015-10-21 16:03:17 +0200

    Regular Expression program added

---
+++ regExpProg.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <regex.h>
+#include <string.h>
+#define LARAALNUM   "^[a-z0-9A-Z_ ]*$"
+
+int main (int argc, char* argv[])
+{
+
+  char input[100 + 1], regExpStr[100 + 1];
+  int ind = 0,retVal = 0, regExpFlag = 0;
+  regex_t re;
+  while (ind < argc)
+  {
+    if (strcmp(argv[ind], "-t")==0)
+    {
+      if (ind < argc+1 && argv[ind+1])
+      {
+	strcpy(input, argv[ind+1]);
+        input[strlen(argv[ind+1])] = '\0';
+      }
+    }
+    if (strcmp(argv[ind], "-r")==0)
+    {
+      if (ind < argc+1 && argv[ind+1])
+      {
+	strcpy(regExpStr, argv[ind+1]);
+        regExpStr[strlen(argv[ind+1])] = '\0';
+        regExpFlag = 1;
+     }
+    }
+
+    ind++;
+  }
+  printf("Input %s, len = %d\n", input,strlen(input));
+
+  if (regcomp(&re, regExpFlag?regExpStr:LARAALNUM, REG_EXTENDED) != 0)
+  {
+    printf("Failed to compile regular exp %s\n", regExpFlag?regExpStr:LARAALNUM);
+    return 0;
+  }
+  
+  retVal = regexec(&re, input, 0, NULL, 0);
+  if (retVal == 0)
+  {
+    printf("Input \"%s\" Confirms to \"%s\" regex\n",input, regExpFlag?regExpStr:LARAALNUM);
+  }
+  else
+    {
+        char errorBuf[100];
+      printf("!!! Input \"%s\" doesnt confirm to \"%s\" regex !!!\n", input, regExpFlag?regExpStr:LARAALNUM);
+      regerror(retVal, &re, errorBuf, 100);
+      printf("Error Information:  %s\n", errorBuf);
+    }
+
+  regfree(&re);
+
+  return 1;
+}
